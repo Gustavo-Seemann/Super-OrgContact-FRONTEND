7d4d2035f930ecddfc6d2846a74390c2da723e32
Luiz Gustavo Seemann <101838119+Gustavo-Seemann@users.noreply.github.com>
2022-11-19 04:16:47 -0300

    feat: login page view

---
+++ src/views/dashboard/contactsView.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Boa tarde {{ userName }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "contactsView",
+
+    data() {
+        return {
+            userName: "Gustavo"
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+div {
+    text-align: start;
+}
+</style>
